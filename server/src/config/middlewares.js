14ea4cd4447c248da0a61b94a789c89ac3eb5562
Thomas Johnson <tjohnson1106@gmail.com>
2018-05-05 20:20:44 -0400

    created7 async authorization fun ction

--- server/src/config/middlewares.js
+++ server/src/config/middlewares.js
@@ -1,3 +1,5 @@
+/*eslint-disable no-param-reassign */
+
 import bodyParser from "body-parser";
 import { graphiqlExpress, graphqlExpress } from "apollo-server-express";
 import { makeExecutableSchema } from "graphql-tools";
@@ -5,11 +7,29 @@ import { makeExecutableSchema } from "graphql-tools";
 import typeDefs from "../graphql/schema";
 import resolvers from "../graphql/resolvers";
 import constants from "./constants";
+import { decodeToken } from "../services/auth";
 
 const schema = makeExecutableSchema({
     typeDefs,
     resolvers
   });
+  
+  
+  async function auth(req, res, next) {
+    try {
+      const token = req.headers.authorization;
+      if (token != null) {
+        const user = await decodeToken(token);
+        req.user = user;
+         
+      } else {
+        req.user = null;
+      }
+    } catch (error) {
+      throw error;
+      
+    }
+  }
 
 
 export default app => {
